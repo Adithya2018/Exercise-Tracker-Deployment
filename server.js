0c6dd90d50119a9a3906f5f31e69884ce6cb17c6
Adithyan S Raj <adithyansraj20@gmail.com>
2020-12-12 14:12:27 +0530

    Update server.js

--- server.js
+++ server.js
@@ -29,10 +29,10 @@ app.use('/users', userRouter);
 // serve static assets if in productiion
 if(process.env.NODE_ENV === 'production'){
     // set static folder
-    app.use(express.static('../frontend/mern-exercise-tracker/build'));
+    app.use(express.static('client/build'));
 
     app.get('*', (req, res) => {
-        res.sendFile(path.resolve(__dirname, '..','frontend','mern-exercise-tracker','build','index.html'));
+        res.sendFile(path.resolve(__dirname, 'client','build','index.html'));
     });
 }
 
